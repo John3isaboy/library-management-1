3e574b513062890d0c758e09d1ce035c3e9f07aa
John3isaboy <john3isaboy@gmail.com>
2026-09-01 17:05:14 +0800

    add AppMain.jave

---
+++ src/com/library/ui/AppMain.java
@@ -0,0 +1,10 @@
+package com.library.ui;
+
+public class AppMain { 
+    public static void main(String[] args) {
+        // Entry point for the application
+        System.out.println("Welcome to the Library Management System!");
+        // Additional initialization code can be added here
+    }
+    
+}
